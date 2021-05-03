93955aff2de1e005b1e96c9f9d1b7b7e7a2e4a93
Henrique Leite <henriqueleite616@gmail.com>
2021-05-03 09:33:48 -0300

    Improve Jest Config

--- src/v1/tests/mocks/repository.ts
+++ src/v1/tests/mocks/repository.ts
@@ -6,25 +6,10 @@ export interface MockRepository {
 	update: jest.Mock<any, any>;
 }
 
-export const makeMockRepository = () => {
-	const mock = {
-		find: jest.fn(),
-		findOne: jest.fn(),
-		save: jest.fn(),
-		insert: jest.fn(),
-		update: jest.fn(),
-	};
-
-	const resetMock = () => {
-		mock.find.mockReset();
-		mock.findOne.mockReset();
-		mock.save.mockReset();
-		mock.insert.mockReset();
-		mock.update.mockReset();
-	};
-
-	return {
-		resetMock,
-		...mock,
-	};
-};
+export const makeMockRepository = (): MockRepository => ({
+	find: jest.fn(),
+	findOne: jest.fn(),
+	save: jest.fn(),
+	insert: jest.fn(),
+	update: jest.fn(),
+});
