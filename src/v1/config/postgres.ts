296e4124bff886798712b901dea6943c2c2ba506
Henrique Leite <henriqueleite616@gmail.com>
2021-05-31 07:30:13 -0300

    Update ESLint Config (#17)

--- src/v1/config/postgres.ts
+++ src/v1/config/postgres.ts
@@ -1,20 +1,21 @@
 import { TypeOrmModule } from "@nestjs/typeorm";
 
 const {
+	NODE_ENV,
 	POSTGRES_HOST,
 	POSTGRES_PASSWORD,
 	POSTGRES_DATABSE,
 	POSTGRES_USER,
 } = process.env;
 
-export const PostgresConnect = TypeOrmModule.forRoot({
+export const POSTGRES_CONNECT = TypeOrmModule.forRoot({
 	type: "postgres",
 	host: POSTGRES_HOST,
 	username: POSTGRES_USER,
 	database: POSTGRES_DATABSE,
 	password: POSTGRES_PASSWORD,
 	synchronize: false,
-	logging: true,
+	logging: NODE_ENV !== "production",
 	migrationsRun: true,
 	entities: ["dist/v1/api/**/*.entity.js"],
 	migrations: ["dist/v1/migrations/*.js"],
