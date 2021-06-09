1b26fb51347b3213842ba672c0e22838f051d103
Henrique Leite <henriqueleite616@gmail.com>
2021-06-09 17:57:51 -0300

    Update Project Config (#18)

--- jest.config.js
+++ jest.config.js
@@ -16,12 +16,14 @@ module.exports = {
 	testEnvironment: "node",
 	moduleDirectories: ["node_modules", "src"],
 	resetMocks: true,
-	// coverageThreshold: {
-	// 	global: {
-	// 		branches: 100,
-	// 		functions: 100,
-	// 		lines: 100,
-	// 		statements: 100,
-	// 	},
-	// },
+	/*
+	 * CoverageThreshold: {
+	 * 	global: {
+	 * 		branches: 100,
+	 * 		functions: 100,
+	 * 		lines: 100,
+	 * 		statements: 100,
+	 * 	},
+	 * },
+	 */
 };
